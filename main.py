44e19466199b03bef385a1cc46e8e51ce617bf9d
Danford-School <58273260+Danford-School@users.noreply.github.com>
2021-01-18 16:06:49 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,16 +3,29 @@
 # Data Gathering program for Breadcrumbs project
 
 # need these
+import json
+
 import requests
 import time
+import kafka
 
 # this is the url to get the huge JSON file
+from kafka import KafkaConsumer, SimpleProducer
+
+
 url = 'http://rbi.ddns.net/getBreadCrumbData'
 # this is to name the file
 today = time.strftime("%Y%m%d")
 # this gets the file from the URL
 # This operation takes a while to download, so be patient if it seems to be hung up
 response = requests.get(url)
+# I am absolutely not sure this is all correct.
+for records in response:
+    kafka = kafka.SimpleClient('10.20.30.12:9092')
+    producer = SimpleProducer(kafka)
+    jd = json.dumps(records)
+    producer.send_messages('sensor-data', jd)
+
 # Creates new file with today's date as the name, writes JSON to the file
-with open(today, 'w') as fw:
-    fw.write(response.text)
+#with open(today, 'w') as fw:
+#    fw.write(response.text)
